c1764abb6fecbc5cd41d45e47e0b1cc0380c32b2
Nicholas Heo <nicholas.heo95@gmail.com>
2020-03-27 13:59:51 -0400

    done

--- server/index.js
+++ server/index.js
@@ -1,28 +1,33 @@
-const express = require('express');
-const mysql = require('mysql');
-const config = require('./config');
-
+const express = require("express");
+const mysql = require("mysql");
+var cors = require("cors");
 const app = express();
-
-  const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'media'
-  });
-
-  connection.connect(err => {
-    if (err) {
-      console.log(err)
-    }
-  });
-
-app.use(express.static('../client/dist'))
-
-app.get('/', (req, res) => {
-  res.send('this works')
+app.use(cors());
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "media"
 });
-
-app.get('/songs', (req, res) => {
+connection.connect(err => {
+  if (err) {
+    console.log(err);
+  }
+});
+var whitelist = ["http://localhost:3306/songs"];
+var corsOptions = {
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+};
+app.use(express.static("../client/dist"));
+app.get("/", cors(corsOptions), (req, res) => {
+  res.send("this works");
+});
+app.get("/songs", (req, res) => {
   connection.query(`SELECT * FROM songs ORDER BY RAND()`, (err, results) => {
     if (err) {
       return res.send(err);
@@ -33,7 +38,6 @@ app.get('/songs', (req, res) => {
     }
   });
 });
-
 app.listen(3306, () => {
-  console.log('media server listening on 3306');
-})
+  console.log("media server listening on 3306");
+});
